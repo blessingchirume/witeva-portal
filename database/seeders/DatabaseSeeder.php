99fb4d3c368d5a8cc70fea4d2161d0f48c9cd735
blessing-chirume <chirume37@gmail.com>
2024-01-23 17:32:31 +0200

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +19,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = new User();
+        $user->create([
+            'name' => 'Blessing',
+            'surname' => 'Chirume',
+            'email' => 'chirume37@gmail.com',
+            'phone' => '0780734018',
+            'password' => Hash::make('12345678'),
+        ]);
     }
 }
