1c7ec6861e6d3455024068001dd4ceff25b0cbf2
blessing-chirume <chirume37@gmail.com>
2024-01-23 22:49:11 +0200

    another hot fix

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect()->route('login');
 });
 
 Route::get('/create', [UserController::class, 'create'])->name('user.create');
