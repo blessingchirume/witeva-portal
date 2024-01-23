681fe90a6d13a26ac5f03220abd38f1adabb9f44
blessing-chirume <chirume37@gmail.com>
2024-01-23 23:37:51 +0200

    Another hot fix

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
+// Route::get('/', function () {
+//     return redirect()->route('login');
+// });
 
 Route::get('/create', [UserController::class, 'create'])->name('user.create');
 
