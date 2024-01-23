c9496b1ae54db6b333cb758bff6f4e64dd384fee
blessing-chirume <chirume37@gmail.com>
2024-01-23 17:37:03 +0200

    changed redirect

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/');
 });
 
 Route::get('/create', [UserController::class, 'create'])->name('user.create');
